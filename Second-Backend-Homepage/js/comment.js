8476c0ebdd5e2ac130015f9f48c39e5f38cd832f
jopemachine <jopemachine@naver.com>
2019-07-03 14:28:51 +0900

    comment style update

--- Second-Backend-Homepage/js/comment.js
+++ Second-Backend-Homepage/js/comment.js
@@ -6,3 +6,25 @@ function hidePlaceholder(){
 $('#CommentArea').click(function(){
   hidePlaceholder();
 });
+
+// 커서를 마지막 위치로 이동시킴
+// 지금 당장은 필요 없지만 댓글에 이미지를 포함시키는 기능을 추가한다면 필요해질 것
+// https://stackoverflow.com/questions/4609405/set-focus-after-last-character-in-text-box
+function focusCampo(id){
+    var inputField = document.getElementById(id);
+    if (inputField != null && inputField.value.length != 0){
+        if (inputField.createTextRange){
+            var FieldRange = inputField.createTextRange();
+            FieldRange.moveStart('character',inputField.value.length);
+            FieldRange.collapse();
+            FieldRange.select();
+        }else if (inputField.selectionStart || inputField.selectionStart == '0') {
+            var elemLen = inputField.value.length;
+            inputField.selectionStart = elemLen;
+            inputField.selectionEnd = elemLen;
+            inputField.focus();
+        }
+    }else{
+        inputField.focus();
+    }
+}
