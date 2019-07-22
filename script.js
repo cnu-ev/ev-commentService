e8335fd0b03165bc92b800a67bd025333adf712a
jopemachine <jopemachine@naver.com>
2019-07-22 20:42:17 +0900

    Update script.js
    evMode의 기본값을 full로 추가했고, 나머지 인자들의 경우 null이 전달되었을 때 서버로 데이터를 전송하지 않도록 했다.

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+var evCommentServiceURL = "https://evcommentservice.ga";
+
 // success시, iframe 태그를 부모 창에 전송함.
 // 성공한 경우에 한정해, 다시 VisitorCounter.php에 데이터를 전송해 방문 수를 체크함
 (async function(){
+
+  // evMode는 기본값으로 full을 갖는다.
+  if(evMode == null) evMode = 'full';
+  // 나머지 인자들은 기본값을 가질 수 없으므로, 해당 인자로 전송된 값이 없다면 서버로 전송하지 않는다.
+  if(blogOwner == null || pageIdentifier == null || siteURL == null || pageTitle == null){
+    return;
+  }
+
   $.ajax({
       crossOrigin: true,
       type: "GET",
-      url : "https://evcommentservice.ga/php-Action/URLVerification.php",
+      url : evCommentServiceURL + "/php-Action/URLVerification.php",
       data : {
           UserName : blogOwner,
           PageIdentifier : pageIdentifier,
@@ -26,7 +36,7 @@
 });
 
 window.addEventListener('message', function(e) {
-  if(e.origin == "https://evcommentservice.ga"){
+  if(e.origin == evCommentServiceURL){
     document.getElementById('EV-Iframe').height = e.data.height;
   }
 });
